4ca77f45a838b8cc7357185b94d327fee3ca6fbf
Oscar <oscar.ramos@ucsp.edu.pe>
2020-05-16 00:46:56 -0500

    Updates

---
+++ precache-manifest.884874444d331899825bded906df271b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46729a1ee0bdbbe729100b236a8c2a03",
+    "url": "/book-finder-app/index.html"
+  },
+  {
+    "revision": "3955bfe4c50a9992a516",
+    "url": "/book-finder-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5721fccd5d5d998e02cd",
+    "url": "/book-finder-app/static/js/2.4c8cf7a4.chunk.js"
+  },
+  {
+    "revision": "b9c4d533931740d7b3b438234c1193fd",
+    "url": "/book-finder-app/static/js/2.4c8cf7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3955bfe4c50a9992a516",
+    "url": "/book-finder-app/static/js/main.2abc64dd.chunk.js"
+  },
+  {
+    "revision": "c9dd8117a25533f98a52",
+    "url": "/book-finder-app/static/js/runtime-main.2c2f12e0.js"
+  }
+]);
